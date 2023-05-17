e70ac777885679102bad06f2addea7f4f3216791
VolodymyrKolomiets <kolomiietsv88@gmail.com>
2023-05-17 09:14:56 +0200

    debug endpoint Order controler create + user Id

--- controllers/OrderController.js
+++ controllers/OrderController.js
@@ -4,7 +4,7 @@ const { Order, OrderProduct } = require('../models/index.js');
 const OrderController = {
     async create(req, res) {
         try {
-            const order = await Order.create(req.body);
+            const order = await Order.create({...req.body,UserId:req.user.id});
             await order.addProduct(req.body.ProductId)
             res.status(201).send({ msg: 'Order creado con exito', order })
         } catch (error) {
